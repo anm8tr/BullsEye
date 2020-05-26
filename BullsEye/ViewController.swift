d6cb859f9cb47006203055ca89d8e067ea9d2eed
Richard Garrison <threeduser@hotmail.com>
2020-05-26 11:17:23 -0700

    Basic controls, Part6 Challenge: Connecting Actions added 2nd button, duplicated IBAction, changed alert message

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -14,7 +14,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showAlert() {
+        
+        let alert = UIAlertController(title: "Hello World!", message: "This is my first app", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func knockKnock() {
+        
+        let alert = UIAlertController(title: "Knock Knock!", message: "Who's there?", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
